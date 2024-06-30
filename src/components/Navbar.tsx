cbf136dc46fec3ac78561a41cf41fd54983d0440
crunchy89 <rocker.hunt@gmail.com>
2024-07-01 00:03:02 +0800

    fix error build

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -41,7 +41,7 @@ const Navbar = () => {
                 <button onClick={model.action.toggleUserMenu} type="button" className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                   <span className="absolute -inset-1.5"></span>
                   <span className="sr-only">Open user menu</span>
-                  <div className="h-10 w-10 rounded-full bg-rose-700 flex items-center justify-center font-bold text-lg text-white">{model.user?.email.charAt(0).toUpperCase()}</div>
+                  <div className="h-10 w-10 rounded-full bg-rose-700 flex items-center justify-center font-bold text-lg text-white">{model.user?.email?.charAt(0).toUpperCase()}</div>
                 </button>
                 <div className={`absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ${model.userMenu ? 'block' : 'hidden'}`} role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex={-1}>
                     {
