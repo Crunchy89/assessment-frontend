16294ab7170b9ba9ab0cce9c3cc6c13c8d748c35
crunchy89 <rocker.hunt@gmail.com>
2024-06-30 23:59:55 +0800

    update Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -35,8 +35,33 @@ const Navbar = () => {
                       </div>
                     </div>
                   </div>
-                  <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                        <UserMenu />
+                  <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0" suppressHydrationWarning={true}>
+                        {/* <UserMenu /> */}
+                        
+                        <div className={`relative ml-3 ${model.isLoggedIn ? "block" : "hidden"}`} suppressHydrationWarning={true}>
+                <button onClick={model.action.toggleUserMenu} type="button" className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
+                  <span className="absolute -inset-1.5"></span>
+                  <span className="sr-only">Open user menu</span>
+                  <div className="h-10 w-10 rounded-full bg-rose-700 flex items-center justify-center font-bold text-lg text-white">{model.user?.email.charAt(0).toUpperCase()}</div>
+                </button>
+                <div className={`absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ${model.userMenu ? 'block' : 'hidden'}`} role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabIndex={-1}>
+                    {
+                        model.defaultUserMenu.map((item) => (
+                            <Link key={item.id} href={item.link} className='block px-4 py-2 text-sm text-gray-700' role='menuitem' tabIndex={-1} id={`user-menu-item-${item.id}`}>
+                                {item.title}
+                            </Link>
+                        ))
+                    }
+                    <button type="button" onClick={model.action.toogleLogout} className="block px-4 py-2 text-sm text-gray-700" tabIndex={-1} id={`user-menu-item-5`}>
+                        Logout
+                    </button>
+                </div>
+            </div>
+
+            <Link href='/login' className={`text-white px-3 py-2 rounded-md text-sm font-medium ${!model.isLoggedIn ? "block" : "hidden"}`}>
+                Login
+            </Link>
+
                     </div>
                 </div>
             </div>
